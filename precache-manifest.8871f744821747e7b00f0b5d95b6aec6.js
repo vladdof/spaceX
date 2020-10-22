6c21f85d3718f06e59d69ed36a9b33a7c8713220
Vladislav Efanov <vladislav.efanov@gmail.com>
2020-10-22 10:26:12 +0200

    forward to the internet

---
+++ precache-manifest.8871f744821747e7b00f0b5d95b6aec6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77230edfaf4a8f12088255b35f9a911a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c7222cc749cd2467b8f",
+    "url": "/static/css/main.fa95aaf6.chunk.css"
+  },
+  {
+    "revision": "17a1223995f44710e75b",
+    "url": "/static/js/2.875d23c4.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/static/js/2.875d23c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c7222cc749cd2467b8f",
+    "url": "/static/js/main.764f782d.chunk.js"
+  },
+  {
+    "revision": "5268b1c7be79030bb5b5",
+    "url": "/static/js/runtime-main.142b1330.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/static/media/logo.d5ab31ce.svg"
+  }
+]);
