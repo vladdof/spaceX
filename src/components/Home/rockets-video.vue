07e10af093afbd7c8e24fcc759e421ea02c2317c
Vladislav Efanov <vladislav.efanov@gmail.com>
2023-09-26 08:24:49 +0200

    feat: change HomeContent cmp

---
+++ src/components/Home/rockets-video.vue
@@ -0,0 +1,7 @@
+<template>
+    <video class="rockets-video" autoplay muted preload="auto">
+        <source src="https://www.spacex.com/media/mission_reusability.mp4" type="video/mp4">
+        <source src="https://www.spacex.com/media/mission_reusability.webm" type="video/webm">
+        Your browser does not support the video tag.
+    </video>
+</template>
