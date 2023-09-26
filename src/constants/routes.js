0afb4f0e7b445f9ad765b0f8203773f6efa231b2
Vladislav Efanov <vladislav.efanov@gmail.com>
2023-09-26 08:16:57 +0200

    feat: add ROUTERS const

---
+++ src/constants/routes.js
@@ -0,0 +1,22 @@
+export const ROUTES = {
+    ROOT: {
+        NAME: 'root',
+        PATH: '/',
+    },
+    CALENDAR: {
+        NAME: 'calendar',
+        PATH: '/calendar',
+    },
+    LAUNCH_ID: {
+        NAME: 'launch',
+        PATH: '/launch/:id',
+    },
+    ROCKET: {
+        NAME: 'rocket',
+        PATH: '/rocket/:name',
+    },
+    NOT_FOUND: {
+        NAME: 'not_found',
+        PATH: '/404',
+    },
+};
