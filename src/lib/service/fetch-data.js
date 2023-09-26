dff965181f9c26ac518e829440446eeb13b35085
Vladislav Efanov <vladislav.efanov@gmail.com>
2023-09-26 08:26:59 +0200

    refactor: change FetchData service

--- src/lib/service/fetch-data.js
+++ src/lib/service/fetch-data.js
@@ -1,11 +1,9 @@
-export default class FetchData {
-    startUrl = 'https://api.spacexdata.com/v4/';
+export class FetchData {
+    startUrl = import.meta.env.VITE_API_SPACE;
 
-    // запрос данных с сервера
     getResource = async url => {
-        // await - пока не получим ответа , не запишем результат в переменную res
         const res = await fetch(url);
-        // у fetch есть результат ok
+
         if (!res.ok) {
             throw new Error(`Произошла ошибка ${res.status}`);
         }
@@ -13,7 +11,7 @@ export default class FetchData {
         return await res.json();
     };
 
-    getRocket = async () => await this.getResource(this.startUrl + 'rockets');
+    getRockets = async () => await this.getResource(this.startUrl + 'rockets');
 
     getLaunches = async () => await this.getResource(this.startUrl + 'launches/past');
 
