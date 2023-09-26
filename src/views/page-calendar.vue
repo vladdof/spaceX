6205db20359d5f11429ec25b49cd96fd12d1ec19
Vladislav Efanov <vladislav.efanov@gmail.com>
2023-09-26 08:15:42 +0200

    feat: add new Pages components

---
+++ src/views/page-calendar.vue
@@ -0,0 +1,15 @@
+<template>
+    <calendar :data="launches" />
+</template>
+
+<script setup>
+    import { storeToRefs } from 'pinia';
+    import { useLaunchesStore } from '../stores';
+    import Calendar from '../components/calendar/calendar.vue';
+
+
+    const store = useLaunchesStore();
+    const { launches } = storeToRefs(store);
+
+    store.fetchLaunches();
+</script>
---
+++ src/views/page-details.vue
@@ -0,0 +1,21 @@
+<template>
+    <launch-details :launch="getLaunchById(id)" />
+</template>
+
+<script setup>
+    import { storeToRefs } from 'pinia';
+    import { useLaunchesStore } from '../stores';
+    import LaunchDetails from '@/components/details/launch-details.vue';
+
+    const { id } = defineProps({
+        id: {
+            type: String,
+            default: '',
+        },
+    });
+
+    const store = useLaunchesStore();
+    const { launches, getLaunchById } = storeToRefs(store);
+
+    if (!launches) store.fetchLaunches();
+</script>
